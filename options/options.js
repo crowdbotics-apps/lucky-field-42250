339c4f1b1251c3c7a3d913ccd5b4294efcc8a185
Crowdbotics <team@crowdbotics.com>
2023-07-18 15:45:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lucky-field-42250",
+  url: "https://lucky-field-42250.botics.co",
+  api: "https://lucky-field-42250.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
